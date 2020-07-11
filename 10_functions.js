83da68b8da72ec3f826bd212ca6b140f4debaf82
Husein <40772085+b-husein@users.noreply.github.com>
2020-07-11 11:07:01 +0300

    added functions.js code

---
+++ 10_functions.js
@@ -0,0 +1,25 @@
+/*
+function name (parameter1, parameter2, parameter3) {
+    // code to be executed;
+}
+*/
+
+// function sayHi() {
+//   return 'Hi there!';
+// }
+
+// console.log(sayHi());
+
+// function sayHi(myDog = 'Silver', myCat = 'Blues', myTurtle = 'Qwerty') {
+//   return 'Hi there ' + myDog + ' ' + myCat + ' ' + myTurtle;
+// }
+
+// console.log(sayHi());
+
+//FUNCTIONS as VARIABLES;
+
+let math = function (a = 4) {
+  return a * a;
+};
+
+console.log(math());
